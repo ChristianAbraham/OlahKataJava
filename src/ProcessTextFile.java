4b289a513ba71727cdd9b65947ef0ebd59052d73
Christian Abraham Putra <90898713+ChristianAbraham@users.noreply.github.com>
2022-12-02 22:02:19 +0700

    added comments to the code

--- src/ProcessTextFile.java
+++ src/ProcessTextFile.java
@@ -2,8 +2,10 @@ import java.io.*;
 
 public class ProcessTextFile {
     private String dataText = "";
-    //read txt file
+
+    //membaca txt file
     public ProcessTextFile(String path){
+        //mencoba untuk membaca file dengan exception handling
         try {
             File file = new File(path);
             BufferedReader br = new BufferedReader(new FileReader(file));
@@ -16,6 +18,7 @@ public class ProcessTextFile {
         }
     }
 
+    //menghapus kata yang duplikat
     public void removeDuplicate(){
         String[] data = this.dataText.split(" ");
         String result = "";
@@ -24,9 +27,10 @@ public class ProcessTextFile {
                 result = result.concat(data[i] + " ");
         }
         this.dataText = result.toLowerCase();
-        System.out.println("Duplicate removed");
+        System.out.println("Duplicate removed"); //menampilkan pesan bahwa kata duplikat telah dihapus
     }
 
+    //menghapus tanda baca
     public void removePunctuation(){
         String result = "";
         for (int i = 0; i < this.dataText.length(); i++) {
@@ -34,7 +38,7 @@ public class ProcessTextFile {
                 result = result.concat(String.valueOf(this.dataText.charAt(i)));
         }
         this.dataText = result;
-        System.out.println("Punctuation removed");
+        System.out.println("Punctuation removed"); //menampilkan pesan bahwa punctuation telah dihapus
     }
 
     //Menghilangkan awalan 
@@ -50,15 +54,18 @@ public class ProcessTextFile {
             result = result.concat(data[i] + " ");
         }
         this.dataText = result;
-        System.out.println("Prefix removed");
+        System.out.println("Prefix removed"); //menampilkan pesan bahwa prefix sudah dihilangkan
     }
 
+    //menghitung jumlah kata
     public void countWord(){
         String[] data = this.dataText.split(" ");
         System.out.println("Number of words: " + data.length);
     }
 
+    //menulis ke hasil proses ke file output.txt
     public void writeFile(){
+        //mencoba untuk menulis file dengan exception handling
         try {
             File file = new File("src\\output.txt");
             BufferedWriter bw = new BufferedWriter(new FileWriter(file));
@@ -67,7 +74,7 @@ public class ProcessTextFile {
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
-        System.out.println("File written");
+        System.out.println("File written"); //menampilkan pesan bahwa file berhasil ditulis
     }
 
 }
