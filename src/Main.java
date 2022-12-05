67af8ea3b17849680548fe811672949e55574212
Christian Abraham Putra <90898713+ChristianAbraham@users.noreply.github.com>
2022-12-05 22:47:33 +0700

    Fix upload saja di hebat

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
+
 public class Main{
     public static void main(String[] args) {
-        ProcessTextFile file = new ProcessTextFile("src\\input.txt");
-        file.removeDuplicate();
-        System.out.println(" ");
-        file.removePunctuation();
-        System.out.println(" ");
+        ProcessTextFile file = new ProcessTextFile("src\\input.txt"); //membaca file input.txt
+
+        System.out.println("Jumlah kata dari file input.txt sebelum diolah: "); //menampilkan pesan jumlah kata sebelum diolah
         file.countWord();
+
+        //Cleaning data
         System.out.println(" ");
-        file.removePrefix();
+        file.removeDuplicate(); //menghapus kata yang duplikat
+
         System.out.println(" ");
-        file.writeFile();
+        file.removePunctuation(); //menghapus tanda baca
+
+        System.out.println(" ");
+        file.removePrefix(); //menghilangkan awalan
+
+        System.out.println(" ");
+        file.writeFile(); //menulis file output.txt
+
+        System.out.println("Jumlah kata dari file input.txt setelah diolah: ");
+        file.countWord(); //menghitung jumlah kata setelah diolah
     }
 }
